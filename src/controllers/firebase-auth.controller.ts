33b6da1d56e3cef75c5ef188579b76b3574abe18
juan <jpyg123@gmail.com>
2024-10-25 23:27:03 -0500

    Changes in save cookie

--- src/controllers/firebase-auth.controller.ts
+++ src/controllers/firebase-auth.controller.ts
@@ -17,6 +17,13 @@ export const Login = async (req: Request, res: Response) => {
         const credential = firebase.auth.GoogleAuthProvider.credential(token);
         const result = await firebase.auth().signInWithCredential(credential);
         const idToken = await result.user.getIdToken();
+        // Guardar el token en una cookie segura
+        res.cookie('jwt', idToken, {
+            httpOnly: true,   
+            secure: true,     
+            sameSite: 'strict', 
+            maxAge: 3600000   
+            });
         res.status(200).send({ token: idToken });
       } catch (error) {
         res.status(401).send({ error: error.message });
@@ -31,15 +38,11 @@ export const Validate = async (req: Request, res: Response) => {
         // Verificar el token del proveedor externo (emitido por Firebase)
         const decodedToken = await admi.auth().verifyIdToken(token);
 
-        // Guardar el token en una cookie segura
-        res.cookie('authToken', token, {
-        httpOnly: true,   // Evita acceso al token desde JavaScript en el cliente
-        secure: true,     // Requiere HTTPS para la cookie
-        sameSite: 'strict', // Protección contra CSRF
-        maxAge: 3600000   // Expiración en 1 hora
-        });
-
-        res.status(200).send({ message: 'Login successful, token set in cookie' });
+        if (!decodedToken) {
+            return res.status(401).send({
+                message: 'unauthenticated'
+            });
+        }
     } catch (error) {
         res.status(401).send({ error: 'Invalid token' });
     }
